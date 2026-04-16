09c772003579a3e65a06439a8030148dd2dfd90a
Luhan Felipe <luhanfelipe28@gmail.com>
2026-04-16 15:32:07 -0300

    Criação da pasta backend

---
+++ backend/src/test/java/com/gametracker/game_tracker/GameTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gametracker.game_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
